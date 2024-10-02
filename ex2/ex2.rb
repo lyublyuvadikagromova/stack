3e5078063f1ce420fd4a94c46fc1d18d8137646b
lyublyuvadikagromova <162696071+lyublyuvadikagromova@users.noreply.github.com>
2024-10-03 01:25:06 +0300

    Add files via upload

--- ex2/ex2.rb
+++ ex2/ex2.rb
@@ -6,11 +6,21 @@ def to_rpn(expression)
     '+' => 1, '-' => 1,
     '*' => 2, '/' => 2
   }
-  expression = expression.gsub('(', ' ( ').gsub(')', ' ) ')
 
-  expression.split.each do |token|
-    if token.match?(/\d+/)
+  expression = expression.gsub('(', ' ( ').gsub(')', ' ) ').gsub(/\s+/, '')
+
+  if expression.match?(/[-+*\/]$/)
+    puts "неповний вираз!"
+    return
+  end
+
+  tokens = expression.scan(/[\d\.]+|[-+*\/()]/)
+  tokens.each_with_index do |token, index|
+    if token.match?(/[\d\.]+/)
       output << token
+    elsif token == '-' && (index == 0 || tokens[index - 1] == '(' || ['+', '-', '*', '/'].include?(tokens[index - 1]))
+      output << '-' + tokens[index + 1]
+      tokens[index + 1] = ''
     elsif ['+', '-', '*', '/'].include?(token)
       while !operators.empty? && precedence[operators.last] && precedence[operators.last] >= precedence[token]
         output << operators.pop
@@ -29,9 +39,17 @@ def to_rpn(expression)
 
   output.join(' ')
 end
+
 puts "введіть вираз:"
 expression = gets.chomp
-puts "зворотний польський запис:"
-puts to_rpn(expression)
+
+if expression.include?('/ 0') || expression.include?('/0')
+  puts "неможливе ділення на нуль!"
+else
+  rpn = to_rpn(expression)
+  puts "зворотний польський запис:"
+  puts rpn
+end
+
 
 
